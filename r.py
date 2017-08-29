f7e8143b0bad0c5ef394eed71d91bcfacf37c99b
Manikandan37 <30549771+Manikandan37@users.noreply.github.com>
2017-08-29 15:02:08 +0530

    Create r.py

---
+++ r.py
@@ -0,0 +1,9 @@
+while True:
+print("enter '0' for exit.")
+ch=input(enter any character:")
+if ch=='0':
+break
+else:
+if(ch=='a' orch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+else:
+print(ch," is not a vowel.\n")
